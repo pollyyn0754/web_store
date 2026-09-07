e66d2aee534b0e14cf05e01825bbdfb4f11fe958
polina dorozhkina <pollyyn0754@gmail.com>
2026-09-07 23:38:10 +0300

    feedback form

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+
+from .forms import ContactForm
 
 
 def home(request):
@@ -6,4 +11,18 @@ def home(request):
 
 
 def contacts(request):
-    return render(request, "contacts.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+            # Добавляем сообщение об успехе
+            messages.success(request, "Ваше сообщение успешно отправлено! Мы свяжемся с вами в ближайшее время.")
+
+            # Перенаправляем на страницу контактов для избежания повторной отправки
+            return HttpResponseRedirect(reverse('catalog:contacts'))
+        else:
+            messages.error(request, "Пожалуйста, исправьте ошибки в форме.")
+    else:
+        form = ContactForm()
+
+    return render(request, "contacts.html", {"form": form})
